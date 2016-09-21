20f7ca45381a5c4cc17badd24018a6d360e8f171
bhauch <bhauch@users.noreply.github.com>
2016-09-21 13:12:43 -0500

    Bugfix
    Routine now ignores 0,0,0 entries from ignored (e.g. too small) marker rows

--- ebsd/getRotation.m
+++ ebsd/getRotation.m
@@ -3,7 +3,7 @@ function rot = getRotation(angfile)
 %  best-fit connecting line, and then computes the angle with the x-axis. 
 %  Angle given in radians
 
-trnsX = angfile.Defect_Centers(:,1);
-trnsY = size(angfile.IQimage,2)-angfile.Defect_Centers(:,2); % image y is "inverted"
+trnsX = angfile.Defect_Centers(angfile.Defect_Centers(:,1)>0,1);
+trnsY = size(angfile.IQimage,2)-angfile.Defect_Centers(angfile.Defect_Centers(:,1)>0,2); % image y is "inverted"
 defectsFit = polyfit(trnsX,trnsY,1); % yields m, b from y=mx+b
 rot = acos(1/sqrt(1+defectsFit(1)^2)); % from dot product
